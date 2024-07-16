1ce1f5e0d0be1cdfcef5327cffa9898ba419cd1f
Diah Putri Lestari <diahputrilestari6@gmail.com>
2024-07-16 08:44:20 +0700

    push shopia

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+
+const Product = (props) => {
+    const {all_product} = useContext(ShopContext);
+    const {ProductId} = useParams();
+    const product = all_product.find((e)=> e.id === Number(ProductId))
+  return (
+    <div>
+        <Breadcrumbs product={product}/>
+        <ProductDisplay product={product}/>
+    </div>
+  )
+}
+
+export default Product
